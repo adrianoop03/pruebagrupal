5e3e0e1addffc59c7f533ff24b9645789cdbf219
LautaroTonini <lautarotonini@gmail.com>
2026-04-13 16:59:13 -0300

    alo

---
+++ clase_cliente.py
@@ -0,0 +1,5 @@
+class Cliente:
+    def __init__ (self, id, medio_de_pago, id_transaccion):
+        self.id = id
+        self.medio_de_pago = medio_de_pago
+        self.id_transaccion = id_transaccion
