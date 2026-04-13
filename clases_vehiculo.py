2c9ebcd379c1260fda308a06eb15afe65b2840cd
adrianoop03 <adrianooyola3@gmail.com>
2026-04-13 17:00:12 -0300

    se sube algo

---
+++ clases_vehiculo.py
@@ -0,0 +1,11 @@
+class Vehiculos:
+    def __init__(self,marca,modelo,color,patente,puertas):
+        self.marca=marca
+        self.modelo=modelo
+        self.color=color
+        self.patente=patente
+        self.puertas=puertas
+        pass
+class Moto(Vehiculos):
+    def __init__(self, marca, modelo, color, patente, puertas):
+        super().__init__(marca, modelo, color, patente, puertas)
